a9654549ad03aad3e532ebee1c7ed7e634bc6db1
Joshua Samuel <joshuasamuel@Joshuas-MacBook-Pro.local>
2026-08-25 22:46:46 +0100

    Configure Sanity Project ID 50173b3c

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "sanity";
 import { schemaTypes } from "./lib/sanity/schemas";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "demo-project-id";
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "50173b3c";
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 
 export default defineConfig({
